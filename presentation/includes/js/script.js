1c35a02f7d246fc112a02ad5bd95ec55bb090f2f
Maxim <maxim@134.155.148.61>
2014-10-23 19:27:11 +0200

    final restructuring

---
+++ presentation/includes/js/script.js
@@ -0,0 +1,9 @@
+/*
+
+My Custom JS
+============
+
+Author:  Maxim Serebrianski
+Updated: 2014-10-12
+
+*/
---
+++ src/com/swt/extractor/CiaExtractor.java
@@ -0,0 +1,10 @@
+package com.swt.extractor;
+
+public class CiaExtractor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
