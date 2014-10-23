f9ea47f82d5208ff18bab7604e4e27b36cdbc785
ms90 <ms_1990@gmx.de>
2014-10-23 17:35:51 +0200

    restructuring

---
+++ src/cia/CIAFB_Extractor.java
@@ -0,0 +1,10 @@
+package cia;
+
+public class CIAFB_Extractor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/dbpedia/DBP_Extractor.java
@@ -0,0 +1,75 @@
+package dbpedia;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+
+
+public class DBP_Extractor {
+	
+	static ArrayList<String> ids = new ArrayList<>();
+
+	public static void main (String args[]) {
+		try {
+			run();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private static void query(String id) throws IOException {
+
+		String queryString = "";
+		
+		Query query = QueryFactory.create(queryString);
+		QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
+		
+		ResultSet rs = qexec.execSelect();
+		if (rs.hasNext()){
+			List<QuerySolution> list = ResultSetFormatter.toList(rs);
+			list.get(0).get("bla");
+		}
+		qexec.close();
+	}
+
+	private static void run() throws IOException {
+		getIds();
+		for (int i=0; i<ids.size(); i++) {
+			System.out.println("Processing " + (i+1) + "/" + ids.size() + " ID: " + ids.get(i));
+			try {
+				query(ids.get(i));
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+	
+	private static void getIds() {
+		try {
+			FileReader fr = new FileReader("Resources/ids.txt");
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			
+			while((line = br.readLine()) != null) {
+				ids.add(line);
+			}
+			br.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
