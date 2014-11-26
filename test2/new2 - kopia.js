b6e8f1479427846bfead152456b0b3c0609cd5d5
ms90 <ms_1990@gmx.de>
2014-11-26 11:36:59 +0100

    sparql fixes

--- test2/new2 - kopia.js
+++ test2/new2 - kopia.js
@@ -1,105 +1,103 @@
-function dbpediaID () {
-  var url = 'http://dbpedia.org/snorql/';
-  var query = '\
-  PREFIX dbpprop: <http://dbpedia.org/property/>\
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\
-PREFIX yago:<http://dbpedia.org/class/yago/>\
-PREFIX dbo: <http://dbpedia.org/ontology/>\
-PREFIX category: <http://dbpedia.org/resource/Category:>\
-PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>\
-\
-SELECT DISTINCT ?countryname ?wikipageID\
-WHERE {\
-    ?country a dbo:Country .\
-    ?country rdfs:label ?enName . \
-    FILTER (langMatches(lang(?enName), "en")) \
-    { ?country dbpprop:iso3166code ?code . }\
-    UNION\
-    { ?country dbpprop:iso31661Alpha ?code . }\
-    UNION\
-    { ?country dbpprop:countryCode ?code . }\
-    UNION\
-    { ?country a yago:MemberStatesOfTheUnitedNations . }\
-    BIND (str(?enName) AS ?countryname)\
-?country dbpedia-owl:wikiPageID ?wikipageID\
-FILTER( xsd:integer(?wikipageID))\
-}';
-
-  var queryUrl = encodeURI( url+"?query="+query+"&format=json" );
-  $.ajax({
-    dataType: "jsonp",
-    url: queryUrl,
-    success: function( _data ) {
-      var results = _data.results.bindings;
-      for ( var i in results ) {
-        var countryname = results[i].countryname.value;
-        var wikipageID = results[i].wikipageID.value;
-        $( 'body' ).append( '<p>'+countryname+':'+ wikipageID+'</p>' );
-      }
-    }
-  });
-
-}
+var dbp = [];
 
 function wikiAttr () {
 //code from http://www.adamtavares.com/story/dbpedia/
-  var url = 'http://dbpedia.org/sparql';
-  var query = '\
-SELECT DISTINCT ?country ?capital ?currency ?language ?population ?gdpnom ?ginii ?areatotal ?govttype ?perofwater \
-?popdencity ?gdppp ?thumb ?wikilink WHERE {\
-\
-?c a dbpedia-owl:Country.\
-?c dbpedia-owl:wikiPageID "11867"^^xsd:integer.\
-?c rdfs:label ?country.\
-FILTER(langMatches(lang(?country), "en"))\
-\
-optional {?c dbpedia-owl:capital ?cap.\
-?cap rdfs:label ?capital.\
-FILTER(langMatches(lang(?capital), "en"))}\
-\
-optional{ ?c dbpedia-owl:currency ?cur.\
-?cur rdfs:label ?currency.\
-FILTER(langMatches(lang(?currency), "en")) }\
-\
-optional{ ?c dbpedia-owl:language ?lang.\
-?lang rdfs:label ?language.\
-FILTER(langMatches(lang(?language), "en"))}\
-\
-optional{ ?c dbpedia-owl:populationTotal ?population.}\
-\
-optional{ ?c dbpprop:gdpNominalPerCapita ?gdpnom.}\
-\
-optional{ ?c dbpprop:gini ?ginii.}\
-\
-optional{ ?c dbpedia-owl:areaTotal ?areatotal.}\
-\
-optional{ ?c dbpedia-owl:governmentType ?gvt.\
-?gvt rdfs:label ?govttype.\
-FILTER(langMatches(lang(?govttype), "en"))}\
- \
-optional{ ?c dbpedia-owl:percentageOfAreaWater ?perofwater.}\
-\
-optional{ ?c dbpedia-owl:populationDensity ?popdencity.}\
-\
-optional{?c dbpprop:gdpPppPerCapita ?gdppp.}\
-\
-optional{ ?c dbpedia-owl:thumbnail ?thumb.}\
-\
-optional{ ?c foaf:isPrimaryTopicOf  ?wikilink.}\
-\
-}';
+var url = 'http://dbpedia.org/sparql';
+    var query = '\
+    SELECT DISTINCT ?country ?capital ?currency ?tongue ?population ?gdpnom ?ginii ?areatotal ?govttype ?perofwater \
+    ?popdensity ?gdppp ?wikilink ?abstract WHERE {\
+    \
+    ?c a dbpedia-owl:Country.\
+    ?c dbpedia-owl:wikiPageID "11867"^^xsd:integer.\
+    ?c rdfs:label ?country.\
+    FILTER(langMatches(lang(?country), "en"))\
+    \
+    optional {?c dbpedia-owl:capital ?cap.\
+    ?cap rdfs:label ?capital.\
+    FILTER(langMatches(lang(?capital), "en"))}\
+    \
+    optional{ ?c dbpedia-owl:currency ?cur.\
+    ?cur rdfs:label ?currency.\
+    FILTER(langMatches(lang(?currency), "en")) }\
+    \
+    optional{ ?c dbpedia-owl:language ?t.\
+    ?t dbpprop:name ?tongue.\
+    FILTER(langMatches(lang(?tongue), "en"))}\
+    \
+    optional{ ?c dbpedia-owl:populationTotal ?population.}\
+    \
+    optional{ ?c dbpprop:gdpNominalPerCapita ?gdpnom.}\
+    \
+    optional{ ?c dbpprop:gini ?ginii.}\
+    \
+    optional{ ?c dbpedia-owl:areaTotal ?areatotal.}\
+    \
+    optional{ ?c dbpedia-owl:governmentType ?gvt.\
+    ?gvt rdfs:label ?govttype.\
+    FILTER(langMatches(lang(?govttype), "en"))}\
+     \
+    optional{ ?c dbpedia-owl:percentageOfAreaWater ?perofwater.}\
+    \
+    optional{ ?c dbpedia-owl:populationDensity ?popdensity.}\
+    \
+    optional{?c dbpprop:gdpPppPerCapita ?gdppp.}\
+    \
+    optional{ ?c foaf:isPrimaryTopicOf  ?wikilink.}\
+    \
+    ?c dbpedia-owl:abstract ?abstract.\
+    FILTER(langMatches(lang(?abstract), "en"))\
+    }';
 
-      var queryUrl = encodeURI( url+"?query="+query+"&format=json" );
-      $.ajax({
-        dataType: "jsonp",
-        url: queryUrl,
-        success: function( _data ) {
-          var results = _data.results.bindings;
-          for ( var i in results ) {
-            var country = results[i].country.value;
-            $( 'body' ).append( '<p>'+country+'"</p>' );
+        var queryUrl = encodeURI( url+"?query="+query+"&format=json" );
+        $.ajax({
+          dataType: "jsonp",
+          url: queryUrl,
+          success: function( _data ) {
+            var results = _data.results.bindings;
+            console.log("Query successful!");
 
+        dbp[0] = results[0].country.value;
+        console.log(dbp[0]);
+
+        dbp[1] = results[0].capital.value;
+        console.log(dbp[1]);
+
+        dbp[2] = results[0].currency.value;
+        console.log(dbp[2]);
+
+        dbp[3] = results[0].tongue.value;
+        console.log(dbp[3]);
+
+        dbp[4] = results[0].population.value;
+        console.log(dbp[4]);
+
+        dbp[5] = results[0].gdpnom.value;
+        console.log(dbp[5]);
+
+        dbp[6] = results[0].ginii.value;
+        console.log(dbp[6]);
+
+        dbp[7] = (results[0].areatotal.value) * Math.pow(10, -6);
+        console.log(dbp[7]);
+
+        dbp[8] = results[0].govttype.value;
+        console.log(dbp[8]);
+
+        dbp[9] = results[0].perofwater.value;
+        console.log(dbp[9]);
+
+        dbp[10] = results[0].popdensity.value;
+        console.log(dbp[10]);
+
+        dbp[11] = results[0].gdppp.value;
+        console.log(dbp[11]);
+
+        dbp[12] = results[0].wikilink.value;
+        console.log(dbp[12]);
+
+        dbp[13] = results[0].abstract.value;
+        console.log(dbp[13]); 
           }
-        }
-      });
+        });
 }
+
